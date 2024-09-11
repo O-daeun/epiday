74fb04b35476bc73570f9031673ea2ce5dc6df8b
daeun <qzsz797979@gmail.com>
2024-09-11 15:37:06 +0900

    init: prettier 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Test() {
   return (
-    <div className='text-2xl'>
+    <div className="text-2xl">
       test
-      <p className='font-bold'>test 홓핳힣켘케케</p>
-      <p className='font-thin font-iropke'>얇은 글씨</p>
+      <p className="font-bold">test 홓핳힣켘케케</p>
+      <p className="text-ellipsis bg-var-black-100 font-iropke font-thin">얇은 글씨</p>
     </div>
   );
 }
