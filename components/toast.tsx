c6c18b9e98be4a453e8dc3f84e910c3d84542a65
daeun <qzsz797979@gmail.com>
2024-09-15 12:05:30 +0900

    chore: heroicons 설치 및 toast에 적용

--- components/toast.tsx
+++ components/toast.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useToastStore } from '@/store/useToastStore';
+import { CheckCircleIcon, ExclamationCircleIcon } from '@heroicons/react/24/outline';
 import { useEffect } from 'react';
 
 export default function Toast() {
@@ -16,8 +17,13 @@ export default function Toast() {
   if (!toast) return null;
   return (
     <div
-      className={`fixed bottom-4 right-4 z-50 overflow-hidden rounded-lg p-4 shadow-lg ${toast.type === 'error' ? 'bg-red-500' : 'bg-green-500'} text-white`}
+      className={`fixed bottom-4 right-4 z-50 flex items-center gap-1 overflow-hidden rounded-lg p-4 shadow-lg ${toast.type === 'error' ? 'bg-red-500' : 'bg-green-500'} text-white`}
     >
+      {toast.type === 'error' ? (
+        <ExclamationCircleIcon className="h-5 w-5 stroke-white" />
+      ) : (
+        <CheckCircleIcon className="h-5 w-5 stroke-white" />
+      )}
       {toast.message}
       <div className="animate-progress absolute bottom-0 left-0 h-1 bg-white"></div>
     </div>
