bc0863d50f695310124e48cfb3da10bd81c658bf
daeun <qzsz797979@gmail.com>
2024-09-18 13:20:16 +0900

    feat: 반응형 공통 layout을 담당하는 InnerLayout 컴포넌트 구현

---
+++ components/inner-layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+  className?: string;
+}
+
+export default function InnerLayout({ children, className }: Props) {
+  return <div className={`mx-auto w-full max-w-[688px] px-6 ${className}`}>{children}</div>;
+}
