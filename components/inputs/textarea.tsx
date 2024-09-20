d203a78efc9ff3a8823917fb6c6bbd806fcdf006
daeun <qzsz797979@gmail.com>
2024-09-20 23:18:43 +0900

    feat: Textarea 컴포넌트 forwardRef 적용 및 error 구현

--- components/inputs/textarea.tsx
+++ components/inputs/textarea.tsx
@@ -1,12 +1,20 @@
-import { TextareaHTMLAttributes } from 'react';
+import { TextareaHTMLAttributes, forwardRef } from 'react';
+import ErrorMessage from './error-message';
 
-interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
-
-export default function Textarea({ ...rest }: Props) {
-  return (
-    <textarea
-      className="h-[148px] w-full rounded-xl border border-var-blue-300 px-4 py-[10px] text-xl outline-none placeholder:text-var-blue-400 focus:border-var-blue-600"
-      {...rest}
-    />
-  );
+interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  error?: string;
 }
+
+const Textarea = forwardRef<HTMLTextAreaElement, Props>(function Textarea({ error, ...rest }, ref) {
+  return (
+    <>
+      <textarea
+        ref={ref}
+        className={`h-[148px] w-full rounded-xl border px-4 py-[10px] text-xl outline-none placeholder:text-var-blue-400 ${error ? 'border-var-error' : 'border-var-blue-300 focus:border-var-blue-600'}`}
+        {...rest}
+      />
+      {error && <ErrorMessage isRight>{error}</ErrorMessage>}
+    </>
+  );
+});
+export default Textarea;
