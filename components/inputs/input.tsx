07c242b5bbc9f123c36af49408fd610ee3d227dc
daeun <qzsz797979@gmail.com>
2024-09-20 23:17:36 +0900

    feat: Input 컴포넌트 forwardRef 및 error 구현

--- components/inputs/input.tsx
+++ components/inputs/input.tsx
@@ -1,14 +1,25 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, forwardRef } from 'react';
+import ErrorMessage from './error-message';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  error?: string;
   className?: string;
 }
 
-export default function Input({ className, ...rest }: Props) {
+const Input = forwardRef<HTMLInputElement, Props>(function Input(
+  { error, className, ...rest },
+  ref,
+) {
   return (
-    <input
-      className={`h-16 w-full rounded-xl border border-var-blue-300 px-4 text-xl outline-none placeholder:text-var-blue-400 focus:border-var-blue-600 ${className}`}
-      {...rest}
-    />
+    <>
+      <input
+        className={`h-16 w-full rounded-xl border px-4 text-xl outline-none placeholder:text-var-blue-400 ${error ? 'border-var-error' : 'border-var-blue-300 focus:border-var-blue-600'} ${className}`}
+        ref={ref}
+        {...rest}
+      />
+      {error && <ErrorMessage isRight>{error}</ErrorMessage>}
+    </>
   );
-}
+});
+
+export default Input;
