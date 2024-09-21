e8b3470dd555405d09a607ed29787a282cfe144d
daeun <qzsz797979@gmail.com>
2024-09-22 08:56:54 +0900

    refactor: Input 컴포넌트에 type 추가

--- components/inputs/input.tsx
+++ components/inputs/input.tsx
@@ -4,15 +4,17 @@ import ErrorMessage from './error-message';
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   error?: string;
   className?: string;
+  type?: string;
 }
 
 const Input = forwardRef<HTMLInputElement, Props>(function Input(
-  { error, className, ...rest },
+  { error, className, type = 'text', ...rest },
   ref,
 ) {
   return (
     <>
       <input
+        type={type}
         className={`h-16 w-full rounded-xl border px-4 text-xl outline-none placeholder:text-var-blue-400 ${error ? 'border-var-error' : 'border-var-blue-300 focus:border-var-blue-600'} ${className}`}
         ref={ref}
         {...rest}
