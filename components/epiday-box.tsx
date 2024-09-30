4c0898b81f0bad6c662cc9ac5e4980b97b8ca6f9
daeun <qzsz797979@gmail.com>
2024-09-30 14:42:12 +0900

    refactor: 피드에 말줄임표 추가 및 디자인 수정보완

--- components/epiday-box.tsx
+++ components/epiday-box.tsx
@@ -9,11 +9,12 @@ interface Props {
 export default function EpidayBox({ epiday }: Props) {
   return (
     <div>
-      <div className='h-[260px] rounded-2xl border border-var-line-100 bg-var-blue-100 bg-[url("/feed-line.svg")] bg-[-50px_-15px] p-6 shadow-[0px_3px_12px_0px_rgba(0,0,0,0.04)]'>
+      <div className='flex h-[260px] rounded-2xl border border-var-line-100 bg-var-blue-100 bg-[url("/feed-line.svg")] bg-[length:auto_100%] bg-[0_-10px] p-6 shadow-[0px_3px_12px_0px_rgba(0,0,0,0.04)]'>
         <EpidayPhrase
           content={epiday.content}
           author={epiday.author}
-          className="h-full justify-between"
+          isContentLimit
+          className="justify-between"
         />
       </div>
       <div className="mt-2 flex justify-end">
--- components/epiday-phrase.tsx
+++ components/epiday-phrase.tsx
@@ -9,6 +9,7 @@ interface Props {
   author: string;
   gap?: 's' | 'm' | 'l';
   authorPosition?: 'left' | 'right';
+  isContentLimit?: boolean;
   className?: string;
 }
 
@@ -17,14 +18,18 @@ export default function EpidayPhrase({
   author,
   gap = 'm',
   authorPosition = 'right',
+  isContentLimit,
   className = '',
 }: Props) {
   const authorName = author.startsWith('본인:') ? author.slice(3) : author;
   const authorPositionStyle = authorPosition === 'right' ? 'text-right' : '';
 
   return (
-    <div className={`flex flex-col gap-8 ${GAP_SIZE[gap]} ${className}`}>
-      <q className="font-iropke text-2xl leading-normal" style={{ quotes: 'none' }}>
+    <div className={`flex w-full flex-col gap-8 ${GAP_SIZE[gap]} ${className}`}>
+      <q
+        className={`break-all font-iropke text-2xl leading-normal ${isContentLimit ? 'line-clamp-4' : ''}`}
+        style={{ quotes: 'none' }}
+      >
         {content}
       </q>
       <cite
