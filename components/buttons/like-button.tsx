d7dea400e3bec3d713aea03f4e2a127ed418fd77
daeun <qzsz797979@gmail.com>
2024-10-02 16:05:50 +0900

    feat: 좋아요 버튼 눌렀을 때 이미 누른 상태면 취소되도록 구현

--- components/buttons/like-button.tsx
+++ components/buttons/like-button.tsx
@@ -9,17 +9,30 @@ import { useState } from 'react';
 interface Props {
   id: number;
   number: number;
+  isInitialLiked: boolean;
 }
 
-export default function LikeButton({ id, number }: Props) {
+export default function LikeButton({ id, number, isInitialLiked }: Props) {
   const { data: session } = useSession();
   const { showToast } = useToastStore();
   const [likeCount, setLikeCount] = useState(number);
+  const [isLiked, setIsLiked] = useState(isInitialLiked);
 
   const handleClick = async () => {
-    const response = await fetchWithToken('POST', `/epigrams/${id}/like`, session);
+    let response;
+    if (isLiked) {
+      response = await fetchWithToken('DELETE', `/epigrams/${id}/like`, session);
+    } else {
+      response = await fetchWithToken('POST', `/epigrams/${id}/like`, session);
+    }
     if (response.ok) {
-      setLikeCount(likeCount + 1);
+      if (isLiked) {
+        setLikeCount(likeCount - 1);
+        setIsLiked(false);
+      } else {
+        setLikeCount(likeCount + 1);
+        setIsLiked(true);
+      }
     } else {
       const { message } = await response.json();
       showToast({ message, type: 'error' });
@@ -29,7 +42,7 @@ export default function LikeButton({ id, number }: Props) {
     <button
       type="button"
       onClick={handleClick}
-      className="flex h-12 w-fit items-center gap-[6px] rounded-full bg-var-black-600 pl-[14px] pr-5 duration-100 ease-in-out hover:shadow-md"
+      className={`flex h-12 w-fit items-center gap-[6px] rounded-full pl-[14px] pr-5 duration-100 ease-in-out hover:shadow-md ${isLiked ? 'bg-var-illust-purple' : 'bg-var-black-600'}`}
     >
       <Image src="/like.svg" width={36} height={36} alt="좋아요" />
       <span className="text-xl font-medium text-var-blue-100">{likeCount}</span>
