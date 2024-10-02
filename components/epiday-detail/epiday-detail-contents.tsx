9ce69c3d9ffbb783b65aee46237ebfea6c10d656
daeun <qzsz797979@gmail.com>
2024-10-02 16:04:58 +0900

    feat: 상세페이지에 출처 링크 추가

--- components/epiday-detail/epiday-detail-contents.tsx
+++ components/epiday-detail/epiday-detail-contents.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { fetchWithToken } from '@/api/fetch-with-token';
+import { REFERENCE_URL_DEFAULT_VALUE } from '@/constants/api-constants';
 import { useToastStore } from '@/store/use-toast-store';
 import { GetEpidayData } from '@/types/epiday-types';
 import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import KebabButton from '../buttons/kebab-button';
 import LikeButton from '../buttons/like-button';
@@ -41,7 +43,7 @@ export default function EpidayDetailContents({ id }: Props) {
   if (!epidayData) return <p>로딩중</p>; // note: 추후 로딩 구현
 
   return (
-    <section className="bg-[url('/detail-contents-bg.png')] bg-cover py-[42px]">
+    <section className="bg-[repeating-linear-gradient(white,white_35px,#F2F2F2_37px)] py-[42px]">
       <InnerLayout>
         <div className="flex items-center justify-between">
           <TagList tags={epidayData.tags} />
@@ -49,9 +51,17 @@ export default function EpidayDetailContents({ id }: Props) {
         </div>
         <EpidayPhrase content={epidayData.content} author={epidayData.author} className="mt-8" />
         <div className="mt-9 flex justify-center gap-[18px]">
-          <LikeButton id={id} number={epidayData.likeCount} />
+          <LikeButton id={id} number={epidayData.likeCount} isInitialLiked={epidayData.isLiked} />
           <ShareButton />
         </div>
+        {epidayData.referenceUrl && epidayData.referenceUrl !== REFERENCE_URL_DEFAULT_VALUE && (
+          <div className="mt-5 flex justify-end gap-1 font-iropke text-var-gray-300">
+            출처:
+            <Link href={epidayData.referenceUrl} className="font-iropke italic hover:underline">
+              {epidayData.referenceTitle ? epidayData.referenceTitle : epidayData.referenceUrl}
+            </Link>
+          </div>
+        )}
       </InnerLayout>
     </section>
   );
