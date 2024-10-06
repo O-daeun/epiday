ac0cd2cb60421817c44045c9f6d207380184637e
daeun <qzsz797979@gmail.com>
2024-10-06 11:59:30 +0900

    refactor: MainSection 최신댓글 반응형을 위해 isResponsive 프롭 추가

--- components/main-section.tsx
+++ components/main-section.tsx
@@ -1,14 +1,29 @@
 import { ReactNode } from 'react';
+import InnerLayout from './inner-layout';
 
 interface Props {
   title: string;
+  isResponsive?: boolean;
   children: ReactNode;
+  className?: string;
 }
 
-export default function MainSection({ title, children }: Props) {
+export default function MainSection({
+  title,
+  isResponsive = false,
+  children,
+  className = '',
+}: Props) {
   return (
-    <section className="flex flex-col gap-10">
-      <h1 className="text-2xl font-semibold">{title}</h1>
+    <section className={`flex flex-col gap-10 ${className}`}>
+      {isResponsive ? (
+        <InnerLayout>
+          <h1 className="text-2xl font-semibold">{title}</h1>
+        </InnerLayout>
+      ) : (
+        <h1 className="text-2xl font-semibold">{title}</h1>
+      )}
+
       {children}
     </section>
   );
