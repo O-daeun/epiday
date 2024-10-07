fcb32ece526bdde92fb63e8d1fc8e5d4b8e609c6
daeun <qzsz797979@gmail.com>
2024-10-07 23:12:53 +0900

    refactor: EpidayBox 태그 리스트 length > 0 일 때만 보여지도록 수정

--- components/epiday-box.tsx
+++ components/epiday-box.tsx
@@ -11,7 +11,7 @@ export default function EpidayBox({ epiday, isContentLimit = false }: Props) {
   return (
     <>
       <div
-        className={`shadow-custom hover:shadow-hover flex rounded-2xl border border-var-line-100 bg-var-blue-100 bg-[repeating-linear-gradient(white,white_25px,#F2F2F2_27px)] p-6 duration-100 ${isContentLimit ? 'h-[260px]' : ''}`}
+        className={`flex rounded-2xl border border-var-line-100 bg-var-blue-100 bg-[repeating-linear-gradient(white,white_25px,#F2F2F2_27px)] p-6 shadow-custom duration-100 hover:shadow-hover ${isContentLimit ? 'h-[260px]' : ''}`}
       >
         <EpidayPhrase
           content={epiday.content}
@@ -20,9 +20,11 @@ export default function EpidayBox({ epiday, isContentLimit = false }: Props) {
           className="justify-between"
         />
       </div>
-      <div className="mt-2 flex justify-end">
-        <TagList tags={epiday.tags} className="font-iropke" />
-      </div>
+      {epiday.tags.length > 0 && (
+        <div className="mt-2 flex justify-end">
+          <TagList tags={epiday.tags} className="font-iropke" />
+        </div>
+      )}
     </>
   );
 }
