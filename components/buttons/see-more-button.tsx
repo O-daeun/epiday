2bbf15d3accde4d9e1b82b249f2db7a26c3b1609
daeun <qzsz797979@gmail.com>
2024-10-20 15:40:39 +0900

    refactor: SeeMoreButton 로딩 중일 때 Prop 추가

--- components/buttons/see-more-button.tsx
+++ components/buttons/see-more-button.tsx
@@ -5,18 +5,25 @@ interface Props {
   onClick: () => void;
   children: ReactNode;
   disabled?: boolean;
+  isLoading?: boolean;
 }
 
-export default function SeeMoreButton({ onClick, disabled, children }: Props) {
+export default function SeeMoreButton({ onClick, disabled, isLoading, children }: Props) {
   return (
     <button
       type="button"
       onClick={onClick}
       disabled={disabled}
-      className="mx-auto mt-20 flex gap-2 rounded-full border border-var-line-200 px-10 py-3 duration-100 hover:shadow-custom"
+      className="mx-auto mt-20 flex items-center gap-2 rounded-full border border-var-line-200 px-10 py-3 duration-100 hover:shadow-custom"
     >
-      <Image src="/plus.svg" width={24} height={24} alt="더보기" />
-      <span className="text-xl font-medium text-var-blue-500">{children}</span>
+      {isLoading ? (
+        <span className="text-xl font-medium text-var-blue-500">로딩 중...</span>
+      ) : (
+        <>
+          <Image src="/plus.svg" width={24} height={24} alt="더보기" />
+          <span className="text-xl font-medium text-var-blue-500">{children}</span>
+        </>
+      )}
     </button>
   );
 }
