58f6ed85e95427d09e1cdf2ec6a9275f1ea47059
daeun <qzsz797979@gmail.com>
2024-10-20 12:37:20 +0900

    design: SideButton 허버 시 shadow 추가

--- components/buttons/side-button.tsx
+++ components/buttons/side-button.tsx
@@ -8,7 +8,7 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 export default function SideButton({ children, isVisible = true, ...rest }: Props) {
   return (
     <button
-      className={`shadow-side flex h-16 min-w-16 items-center justify-center gap-1 rounded-full bg-var-blue-900 px-[21px] text-xl font-semibold text-var-blue-100 duration-300 ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}
+      className={`shadow-side hover:shadow-sideHover flex h-16 min-w-16 items-center justify-center gap-1 rounded-full bg-var-blue-900 px-[21px] text-xl font-semibold text-var-blue-100 duration-300 ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}
       {...rest}
     >
       {children}
