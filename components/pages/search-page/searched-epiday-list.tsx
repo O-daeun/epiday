00c2c3c34d784449b139f04124c42e803f88bdce
daeun <qzsz797979@gmail.com>
2024-10-23 13:48:56 +0900

    refactor: Search페이지 react-query로 마이그레이션

--- components/pages/search-page/searched-epiday-list.tsx
+++ components/pages/search-page/searched-epiday-list.tsx
@@ -1,90 +1,64 @@
-import { fetchWithoutToken } from '@/api/fetch-without-token';
+import { getEpidays } from '@/api/epiday/get-epidays';
+import { queryKeys } from '@/constants/query-keys';
 import { useObserver } from '@/hooks/use-observer';
-import { useToastStore } from '@/store/use-toast-store';
 import { GetEpidaysData } from '@/types/epiday-types';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import Link from 'next/link';
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import EpidayPhrase from '../../epiday-phrase';
 import TagList from '../../tag-list';
 import HighlightKeyword from './highlight-keyword';
+
+const limit = 8;
 
 interface Props {
   keyword: string;
 }
 
 export default function SearchedEpidayList({ keyword }: Props) {
-  const [epidaysData, setEpidaysData] = useState<GetEpidaysData>();
-  const [cursor, setCursor] = useState<number | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(true);
-  const { showToast } = useToastStore();
   const observerRef = useRef<HTMLDivElement | null>(null);
 
-  const fetchEpidays = async (isInitialLoad = false) => {
-    if (!hasMore || isLoading) return;
-    setIsLoading(true);
-
-    const cursorQuery = cursor && !isInitialLoad ? `&cursor=${cursor}` : '';
-    const response = await fetchWithoutToken(
-      'GET',
-      `/epigrams?limit=3&keyword=${keyword}${cursorQuery}`,
-    );
-
-    if (response.ok) {
-      const data = await response.json();
-
-      if (isInitialLoad) {
-        setEpidaysData(data);
-      } else {
-        setEpidaysData((prev) => ({
-          ...data,
-          list: prev ? [...prev.list, ...data.list] : data.list,
-        }));
-      }
-
-      if (data.list.length > 0) {
-        const lastEpiday = data.list[data.list.length - 1];
-        setCursor(lastEpiday.id);
-      } else {
-        setHasMore(false);
-      }
-    } else {
-      const { message } = await response.json();
-      showToast({ message, type: 'error' });
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    setCursor(null);
-    setEpidaysData(null);
-    setHasMore(true);
-    fetchEpidays(true);
-  }, [keyword]);
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading, isError, refetch } =
+    useInfiniteQuery<GetEpidaysData>({
+      queryKey: queryKeys.epiday.searchedEpidays(keyword),
+      queryFn: ({ pageParam = '' }) => getEpidays({ limit, pageParam, keyword }),
+      getNextPageParam: (lastPage) =>
+        lastPage.list.length > 0 ? lastPage.list[lastPage.list.length - 1].id : null,
+      staleTime: 1000 * 60 * 10,
+      refetchOnWindowFocus: false,
+      initialPageParam: '',
+    });
 
   useObserver({
-    isLoading,
+    isLoading: isFetchingNextPage,
     ref: observerRef,
-    fetchNextCursor: () => fetchEpidays(false),
+    fetchNextCursor: () => {
+      if (hasNextPage) fetchNextPage(); // 다음 페이지가 존재하면 추가로 데이터 로드
+    },
   });
+
+  if (isLoading) return <p>Loading...</p>; // note: 로딩구현
+  if (isError) return <p>Error</p>; // note: 에러 구현
 
   return (
     <ul className="mx-auto mt-10 max-w-[640px]">
-      {epidaysData?.list.map((epiday) => (
-        <li key={epiday.id} className="border-b border-var-gray-100">
-          <Link href={`/epidays/${epiday.id}`} className="block p-6">
-            <EpidayPhrase
-              content={<HighlightKeyword text={epiday.content} keyword={keyword} />}
-              author={epiday.author}
-              gap="m"
-              authorPosition="left"
-            />
-            <div className="mt-4 flex justify-end">
-              <TagList tags={epiday.tags} keyword={keyword} />
-            </div>
-          </Link>
-        </li>
-      ))}
+      {data?.pages.map((page) =>
+        page.list.map((epiday) => (
+          <li key={epiday.id} className="border-b border-var-gray-100">
+            <Link href={`/epidays/${epiday.id}`} className="block p-6">
+              <EpidayPhrase
+                content={<HighlightKeyword text={epiday.content} keyword={keyword} />}
+                author={epiday.author}
+                gap="m"
+                authorPosition="left"
+              />
+              <div className="mt-4 flex justify-end">
+                <TagList tags={epiday.tags} keyword={keyword} />
+              </div>
+            </Link>
+          </li>
+        )),
+      )}
       <div ref={observerRef} />
     </ul>
   );
