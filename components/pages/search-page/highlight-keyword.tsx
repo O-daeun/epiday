fab638e91c1ac6c664da5f3b9c6a7c48efefc655
daeun <qzsz797979@gmail.com>
2024-10-23 13:49:59 +0900

    fix: HighlistKeywords 함수에 특수문자 포함 시 정규식이 에러나는 현상 수정

--- components/pages/search-page/highlight-keyword.tsx
+++ components/pages/search-page/highlight-keyword.tsx
@@ -3,19 +3,28 @@ interface Props {
   keyword: string;
 }
 
-export default function HighlightKeyword({ text, keyword }: Props) {
-  if (!keyword) return text;
+function escapeRegExp(string: string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // 정규식 특수문자들을 escape 처리
+}
 
-  const regex = new RegExp(`(${keyword})`, 'gi');
+export default function HighlightKeyword({ text, keyword }: Props) {
+  if (!keyword) return <>{text}</>;
+
+  const escapedKeyword = escapeRegExp(keyword);
+  const regex = new RegExp(`(${escapedKeyword})`, 'gi');
   const parts = text.split(regex);
 
-  return parts.map((part, i) =>
-    regex.test(part) ? (
-      <span key={i} className="font-[inherit] text-var-illust-blue">
-        {part}
-      </span>
-    ) : (
-      part
-    ),
+  return (
+    <>
+      {parts.map((part, i) =>
+        regex.test(part) ? (
+          <span key={i} className="font-[inherit] text-var-illust-blue">
+            {part}
+          </span>
+        ) : (
+          part
+        ),
+      )}
+    </>
   );
 }
