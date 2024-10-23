cfc50638ed4b64be4c7c5ff0dd67faf6757a7f1a
daeun <qzsz797979@gmail.com>
2024-10-23 16:03:49 +0900

    rename: getRecentComments 함수 getComments로 이름 변경

--- api/comment/get-comments.ts
+++ api/comment/get-comments.ts
@@ -1,10 +1,15 @@
 import { GetCommentsData } from '@/types/comment-types';
 import { fetchWithoutToken } from '../fetch-without-token';
 
-export const getRecentComments = async (
-  limit: number,
-  { pageParam = '' },
-): Promise<GetCommentsData> => {
+interface GetCommentsParams {
+  limit: number;
+  pageParam?: string | unknown;
+}
+
+export const getComments = async ({
+  limit,
+  pageParam = '',
+}: GetCommentsParams): Promise<GetCommentsData> => {
   const cursorParams = pageParam ? `&cursor=${pageParam}` : '';
   const response = await fetchWithoutToken('GET', `/comments?limit=${limit}${cursorParams}`);
 
