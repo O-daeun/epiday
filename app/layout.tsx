c4d4f52cd0ecd6ed42df4ba6e6138ebf5f741e17
daeun <qzsz797979@gmail.com>
2024-10-29 13:19:35 +0900

    feat: 메타데이터 OpenGraph 관련 정보 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,22 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.ico',
   },
+  openGraph: {
+    title: 'Epiday',
+    description: '감정상태에 따른 명언과 글귀들을 열람하고 공유하는 서비스',
+    url: 'https://epiday.vercel.app',
+    siteName: 'Epiday',
+    images: [
+      {
+        url: '/metadata.svg',
+        width: 1200,
+        height: 900,
+        alt: 'Epiday 이미지',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
 };
 
 interface Props {
